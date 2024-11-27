debb79de041f35a43472026b497d34644bae9394
V.T <162502425+vivektalpada07@users.noreply.github.com>
2024-11-27 16:18:17 +1300

    Delete frontend/quiz-app directory

--- frontend/quiz-app/src/services/PlayerService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8080/api/quizzes'; 
-
-const getQuizzes = () => axios.get(`${API_URL}/ongoing`); 
-const getQuizDetails = (id) => axios.get(`${API_URL}/${id}`); 
-
-export { getQuizzes, getQuizDetails };
